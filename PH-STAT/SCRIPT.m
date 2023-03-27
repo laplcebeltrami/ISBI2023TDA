03f5adbb86e8c54cb3a280f0f9f6d5bdf68151fe
laplcebeltrami <68470151+laplcebeltrami@users.noreply.github.com>
2023-03-27 06:13:37 -0500

    Add files via upload

--- PH-STAT/SCRIPT.m
+++ PH-STAT/SCRIPT.m
@@ -2,11 +2,13 @@
 %
 % https://github.com/laplcebeltrami/ISBI2023TDA/blob/main/PH-STAT
 %
-% (C) 2023 Moo K. Chung, Universtiy of Wisconsin-Madison 
+% (C) 2023 Moo K. Chung, Zijian Chen
+% Universtiy of Wisconsin-Madison 
 %
-%     Email: mkchung@wisc.edu
+% email: mkchung@wisc.edu
 
 %% --------------------
+%% Rips complex
 %genrate random p number of scatter points in [0,1]^d hyper-cube.
 %X is the matrix of size p x d of coordinates
 p=50; d=3;
@@ -23,61 +25,37 @@ PH_rips_display(X,S)
 %labels = cellstr(num2str((1:p)', '% d'));
 %text(X(:,1)+0.01, X(:,2)+0.01, X(:,3)+0.01, labels, 'Color', 'r', 'FontSize',16) 
 
-%Boundary matrices
+%% -----------------
+%% Boundary matrices
 B = PH_boundary(S);
 betti = PH_betti(B)
 title('\beta_0=1, \beta_1=4, \beta_2=0')
 
 
-%% -----------------------
-%genrate random p number of scatter points along a circle in [0,1]^d hyper-cube.
-%X is the matrix of size p x d of coordinates
-p=50; d=3;
-c = [0.5, 0.5, 0.5];
-theta = linspace(0, 2*pi, p)';
+%% ---------------
+%% Sampling from a single Dirichlet distribution 
 
-x = c(1) + 0.4*cos(theta);
-y = c(2) + 0.4*sin(theta);
-z = c(3) + zeros(size(theta));
-X = [x y z] + rand(p, d)/2; % add a noise to make them affinely independent. 
+alpha=[3,2,3]';
+weight=1;
+n=500;
 
-%Compute the Rips complex up to 3-simplices.
-S= PH_rips(X, 4, 0.3)
-
-%Display Ris complex
-PH_rips_display(X,S)
-%labels = cellstr(num2str((1:p)', '% d'));
-%text(X(:,1)+0.01, X(:,2)+0.01, X(:,3)+0.01, labels, 'Color', 'r', 'FontSize',18) 
-
-%Boundary matrices
-B = PH_boundary(S);
-betti = PH_betti(B)
-title('\beta_0=1, \beta_1=5, \beta_2=0')
+figure;
+Dirichelt_theoretical(alpha,weight);
+x = Dirichlet_sample(alpha,weight,n);
+hold on; scatter(x(:,1),x(:,2),100,'.k','MarkerFaceAlpha',0.05);
 
 
-%%  ------------------
-%Icosahedron: A solid with 20 equilateral triangular faces, 12 vertices, and 30 edges.
+%% ----------------
+%% Sampling from a mixture of Dirichlet distributions
 
-phi = (1 + sqrt(5)) / 2;  % Golden ratio
-% Define the coordinates of the vertices
-X = [0.5 + 0.5/phi, 0.5 + 0.5/phi, 0;
-            0.5 + 0.5/phi, -0.5 - 0.5/phi, 0;
-            -0.5 - 0.5/phi, 0.5 + 0.5/phi, 0;
-            -0.5 - 0.5/phi, -0.5 - 0.5/phi, 0;
-            0, 0.5 + 0.5/phi, 0.5 + 0.5/phi;
-            0, 0.5 + 0.5/phi, -0.5 - 0.5/phi;
-            0, -0.5 - 0.5/phi, 0.5 + 0.5/phi;
-            0, -0.5 - 0.5/phi, -0.5 - 0.5/phi;
-            0.5 + 0.5/phi, 0, 0.5 + 0.5/phi;
-            0.5 + 0.5/phi, 0, -0.5 - 0.5/phi;
-            -0.5 - 0.5/phi, 0, 0.5 + 0.5/phi;
-            -0.5 - 0.5/phi, 0, -0.5 - 0.5/phi];
+alpha=[3,8,2;8,3,2;7,3,8;3,7,8]';
+weight = [0.25,0.25,0.25,0.25];
+n = 500;
 
-S= PH_rips(X, 4, 1.8)
+figure;
+Dirichelt_theoretical(alpha,weight);
+s = Dirichlet_sample(alpha,weight,n);
+hold on; scatter(s(:,1),s(:,2),100,'.k');
 
-%Display Ris complex
-PH_rips_display(X,S)
 
-B = PH_boundary(S);
-betti = PH_betti(B)
 
